8c1068598ea3d185d8a94e77ecb71d4fd37cafbb
viraj <virajshah1605@gmail.com>
2025-08-13 23:38:53 +0530

    Reset Project

--- hackmate_backend/hackmate_backend/settings.py
+++ hackmate_backend/hackmate_backend/settings.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
 
 # --- JWT token lifetime ---
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
